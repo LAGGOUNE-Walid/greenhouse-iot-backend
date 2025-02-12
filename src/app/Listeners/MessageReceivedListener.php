b9a077e11e506e1c1d107c031d4bed426d01df68
LAGGOUNE-Walid <walidlaggoune159@gmail.com>
2025-02-12 13:16:18 +0100

    fixed soild factory bug

--- src/app/Listeners/MessageReceivedListener.php
+++ src/app/Listeners/MessageReceivedListener.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\MqttAirMessageReceived;
 use App\Events\MqttSoilMessageReceived;
 use App\Mqtt\Factory\AirHandlerFactory;
+use App\Mqtt\Factory\SoilHandlerFactory;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Mqtt\Handlers\SoilMoistureHandler;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -26,7 +27,7 @@ class MessageReceivedListener
     {
         $handlerFactory = match (get_class($event)) {
             MqttAirMessageReceived::class => new AirHandlerFactory,
-            MqttSoilMessageReceived::class => new SoilMoistureHandler,
+            MqttSoilMessageReceived::class => new SoilHandlerFactory,
         };
         if (! json_validate($event->message)) {
             throw new \Exception("Message $event->message is not a valid json");
