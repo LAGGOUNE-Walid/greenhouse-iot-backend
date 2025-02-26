36445a9a09653949868c9dfa5f62ec3cf6c63c7a
LAGGOUNE-Walid <walidlaggoune159@gmail.com>
2025-02-26 13:57:21 +0100

    fixed bug

--- src/app/Mqtt/Handlers/BatteryHandler.php
+++ src/app/Mqtt/Handlers/BatteryHandler.php
@@ -5,11 +5,12 @@ namespace App\Mqtt\Handlers;
 use App\Models\Measurement;
 use App\Contracts\MqttMessage;
 use App\Contracts\TopicHandlerInterface;
+use App\Models\BatteryLevel;
 
 class BatteryHandler implements TopicHandlerInterface
 {
     public function save(MqttMessage $message): bool
     {
-        return Measurement::create($message->toElequentArray());
+        return BatteryLevel::create($message->toElequentArray());
     }
 }
