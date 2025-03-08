8ad1147bb9159353a009cb028879b94ec5029cf6
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2025-03-08 18:06:44 +0100

    Running mqtt listen commands as docker services

--- src/app/Listeners/BatteryMessageReceivedListener.php
+++ src/app/Listeners/BatteryMessageReceivedListener.php
@@ -2,9 +2,11 @@
 
 namespace App\Listeners;
 
+use App\Mqtt\Handlers\BatteryHandler;
+use Illuminate\Queue\InteractsWithQueue;
 use App\Events\MqttBatteryMessageReceived;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use App\Mqtt\Factory\BatteryLevelHandlerFactory;
 
 class BatteryMessageReceivedListener
 {
@@ -23,7 +25,10 @@ class BatteryMessageReceivedListener
      */
     public function handle(MqttBatteryMessageReceived $event): void
     {
-        //
-        dump($event);
+        $handlerFactory = new BatteryLevelHandlerFactory;
+        if (! json_validate($event->message)) {
+            throw new \Exception("Message $event->message is not a valid json");
+        }
+        $handlerFactory->handle(json_decode($event->message, true));
     }
 }
