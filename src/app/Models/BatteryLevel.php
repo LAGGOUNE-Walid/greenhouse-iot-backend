8f02431b01baefd9fc11133d2d7de6a33ff236dd
LAGGOUNE-Walid <walidlaggoune159@gmail.com>
2025-04-30 11:59:55 +0100

    limit battery level at 100

--- src/app/Models/BatteryLevel.php
+++ src/app/Models/BatteryLevel.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BatteryLevel extends Model
 {
     protected $fillable = ['node_id', 'value'];
+
+    protected function value(): Attribute
+
+    {
+        return Attribute::make(
+            get: fn (int $value) =>  max(0, min(100, $value)),
+        );
+
+    }
 }
